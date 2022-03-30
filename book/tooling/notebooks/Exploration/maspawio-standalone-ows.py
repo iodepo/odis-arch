ac36422204bfcef19eee84ef2db1ed212b2e9e82
jmckenna <jmckenna@gatewaygeomatics.com>
2022-03-30 10:47:18 -0300

    update CSW parser

--- book/tooling/notebooks/Exploration/maspawio-standalone-ows.py
+++ book/tooling/notebooks/Exploration/maspawio-standalone-ows.py
@@ -72,7 +72,7 @@ csw = CatalogueServiceWeb(CSW_ENDPOINT)
 #['GetCapabilities', 'GetRecords', 'GetRecordById', 'DescribeRecord', 'GetDomain']
 #csw.getdomain('GetRecords.resultType')
 #print(csw.results)
-csw.getrecords2(esn="full", resulttype="results", typenames='gmd:MD_Metadata')
+csw.getrecords2(esn="full", resulttype="hits", typenames='gmd:MD_Metadata')
 nrecords = len(csw.records)
 print(str(nrecords) + " records found...")
 index = 0
