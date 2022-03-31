0298fe594f3763ff655620d9c3046c6e395b86b1
jmckenna <jmckenna@gatewaygeomatics.com>
2022-03-31 16:39:22 -0300

    update CSW parser

--- book/tooling/notebooks/Exploration/maspawio-standalone-ows.py
+++ book/tooling/notebooks/Exploration/maspawio-standalone-ows.py
@@ -178,7 +178,7 @@ while stop == 0:
             # keyword(s) loop
             k = []
             #print(*subjects)
-            print(subjects[0]['keywods'])
+            print(subjects[0]['keywords'])
             for s in subjects:
                 k.append(s)
             data['https://schema.org/keywords'] = k 
