c09fce076ba1ecd8f52c6242345846d5c5749e22
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2024-05-06 10:20:38 -0300

    add lastmod date for WOD (issue #126 )

--- collection/scripts/generate_sitemap.php
+++ collection/scripts/generate_sitemap.php
@@ -11,8 +11,8 @@
    History:         originally created for COINAtlantic catalogue in 2011
 */
 
-define ('JSONLD_ROOT_PATH_DIRECTORY', '/home/apps/odis-arch-git/collection/tempHosting/data-acma');
-define ('JSONLD_ROOT_PATH_URL', 'https://raw.githubusercontent.com/iodepo/odis-arch/master/collection/tempHosting/data-acma');
+define ('JSONLD_ROOT_PATH_DIRECTORY', '/home/apps/odis-arch-git/collection/tempHosting/data-wod');
+define ('JSONLD_ROOT_PATH_URL', 'https://raw.githubusercontent.com/iodepo/odis-arch/master/collection/tempHosting/data-wod');
 
 $basedir = JSONLD_ROOT_PATH_DIRECTORY;
 $baseurl = JSONLD_ROOT_PATH_URL;
@@ -26,18 +26,18 @@ function getdepth($fn){
 function printlink($fn)
 {
     $url = str_replace(JSONLD_ROOT_PATH_DIRECTORY, JSONLD_ROOT_PATH_URL, $fn);
+    $currentDate = date('Y-m-d');    
     echo "  <url>" . "\n";
     echo "    <loc>$url</loc>" . "\n";
+    echo "    <lastmod>$currentDate</lastmod>" . "\n";
+    echo "    <changefreq>monthly</changefreq>" . "\n";
     echo "  </url>" . "\n";
     return;
-
-
 }
 
 // main function that scans the directory tree
 function listdir($basedir)
 {
-
     if ($handle = @opendir($basedir)) 
     { 
         while (false !== ($fn = readdir($handle))){ 
